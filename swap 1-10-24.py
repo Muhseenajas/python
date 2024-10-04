c0f2a0300b10888b8d7b96da4968ae7ab965f00a
Muhseenajas <muhseenajas2003@gmail.com>
2024-10-04 07:03:44 +0100

    Add files via upload

---
+++ swap 1-10-24.py
@@ -0,0 +1,7 @@
+x=int(input("enter the 1st num:"))
+y=int(input("enter the 2nd num:"))
+print("before swaping:")
+print("value of x:",x, "and y:",y)
+x,y=y,x
+print("after swaping:")
+print("value of x:",x, "and y:",y)
