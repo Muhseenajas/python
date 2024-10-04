68cef7cad6e805aa6e6b55658f907fb2499665b0
Muhseenajas <muhseenajas2003@gmail.com>
2024-10-04 07:04:41 +0100

    Add files via upload

---
+++ totalmark 1-10-24.py
@@ -0,0 +1,9 @@
+s1=int(input("enter the mark of 1st sub:"))
+s2=int(input("enter the mark of 2nd sub:"))
+s3=int(input("enter the mark of 3rd sub:"))
+s4=int(input("enter the mark of 4th sub:"))
+s5=int(input("enter the mark of 5th sub:"))
+total=s1+s2+s3+s4+s5
+per=(total/500)*100
+print (total)
+print (per)
