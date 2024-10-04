86f72d8b3a45fe360c9d0b455a622b0fa1971e71
Muhseenajas <muhseenajas2003@gmail.com>
2024-10-04 07:29:09 +0100

    Update arithmetic27-9-24.py

--- arithmetic27-9-24.py
+++ arithmetic27-9-24.py
@@ -1,6 +1,10 @@
-a=10
-b=20
-print(a+b)
-print(a/b)
-print(a*b)
-print(a-b)
+a=int(input("enter 1st no:"))
+b=int(input("enter 2nd no:"))
+sum=a+b
+print(sum)
+dif=a-b
+print(dif)
+mul=a*b
+print(mul)
+div=a/b
+print(div)
