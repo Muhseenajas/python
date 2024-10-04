b701a6a945676f9da70123dabb3e5aa962201227
Muhseenajas <muhseenajas2003@gmail.com>
2024-10-04 07:38:32 +0100

    Update swap 1-10-24.py

--- swap 1-10-24.py
+++ swap 1-10-24.py
@@ -2,6 +2,8 @@ x=int(input("enter the 1st num:"))
 y=int(input("enter the 2nd num:"))
 print("before swaping:")
 print("value of x:",x, "and y:",y)
-x,y=y,x
+x=x+y
+y=x-y
+x=x-y
 print("after swaping:")
 print("value of x:",x, "and y:",y)
