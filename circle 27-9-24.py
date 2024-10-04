4b4cf4c4ff001cca4be0b9ed77d35043fb7668ff
Muhseenajas <muhseenajas2003@gmail.com>
2024-10-04 07:31:12 +0100

    Add files via upload

---
+++ circle 27-9-24.py
@@ -0,0 +1,13 @@
+r=int(input("enter the radius of the circle"))
+area=3.14*r*r
+print(area)
+
+l=int(input("enter the length of rectangle:"))
+b=int(input("enter the breadth of rectangle:"))
+recarea=l*b
+print(recarea)
+
+h=int(input("enter the height of traingle:"))
+b=int(input("enter the breadth of triangle:"))
+triaarea=1/2*b*h
+print(triaarea)
