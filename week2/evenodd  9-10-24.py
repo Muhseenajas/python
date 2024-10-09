d1055d20027914eda7bb03144077130414e2f783
Muhseenajas <muhseenajas2003@gmail.com>
2024-10-09 06:55:20 +0100

    Add files via upload

---
+++ week2/evenodd  9-10-24.py
@@ -0,0 +1,5 @@
+n=int(input("enter number:"))
+if(n%2==0):
+    print(n,"number is even")
+else:
+    print(n,"number is odd")
---
+++ week2/factorial 9-10-24.py
@@ -0,0 +1,5 @@
+a=int(input("enter the number:"))
+factorial=1
+for i in range(1,a+1):
+    factorial *= i
+print("the factorial of",a," is",factorial)
