30dd76c80c6379233a95b8c55a64dded73aff3e9
Muhseenajas <muhseenajas2003@gmail.com>
2025-01-19 13:46:51 +0530

    Create main.py

---
+++ december/week1/package/main.py
@@ -0,0 +1,17 @@
+from graphics.circle import *
+from graphics.rectangle import *
+from graphics.threed.cuboid import *
+from graphics.threed.sphere import *
+from graphics.threed.triangle import *
+
+print("area of circle:",circle_area(3))
+print("perimeter of circle:",circle_perimeter(3))
+print("area of rectangle:",rectangle_area(2,3))
+print("perimeter of rectangle:",rectangle_perimeter(2,3))
+print("area of cuboid:",cuboid_area(3,2,3))
+print("perimeter of cuboid:",cuboid_perimeter(3,2,3))
+print("area of sphere:",sphere_area(2))
+print("perimeter of sphere:",sphere_perimeter(2))
+print("area of triangle:",triangle_area(2,3))
+print("perimeter  of triangle:",triangle_peri(2,3,4))
+
