54e23ed6095d5686b7b9f8fb87509e8169bb532f
Muhseenajas <muhseenajas2003@gmail.com>
2025-01-19 13:50:53 +0530

    Create sphere.py

---
+++ december/week1/package/graphics/threed/sphere.py
@@ -0,0 +1,8 @@
+def sphere_area(radius):
+    from math import pi
+    return 4*pi*radius*radius
+def sphere_perimeter(radius):
+    from math import pi
+    return (4/3)*pi*radius*radius*radius
+
+
